0dce6bb1832f9f0e5698b6ef084c6034aac36bfa
Anders Blehr <anders@andersblehr.co>
2018-11-18 08:39:42 +0100

    Align directory structure with logical folder structure (thx, venmo/synx)

--- SheetMusicPlayerTests/SheetMusicPlayerTests.h
+++ SheetMusicPlayerTests/SheetMusicPlayerTests.h
@@ -6,10 +6,10 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
 
-@interface SheetMusicPlayerTests : SenTestCase {
+@interface SheetMusicPlayerTests : XCTestCase {
 @private
     
 }
